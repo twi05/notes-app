657bd427dd937db4464e609639753f633c5e8d6a
Twinshu <twinshup@gmail.com>
2022-06-19 00:45:01 +0530

    intial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import {MdDeleteForever} from 'react-icons/md'
+const Note = () => {
+  return (
+    <div>
+        <div className='note'>
+            <span>Hello this is notes data</span>
+            <div className='note-footer'></div>
+            <small>13/05/2021</small>
+        </div>
+
+    </div>
+  )
+}
+
+export default Note
---
+++ src/components/Notes_list.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Note from './Note'
+
+const Notes_list = () => {
+  return (
+    <div>
+        
+        <Note/>
+    </div>
+  )
+}
+
+export default Notes_list
